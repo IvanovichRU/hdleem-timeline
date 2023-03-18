6e306a569806843b3bdb797229b77de55bd05dd3
IvanovichRU <kevinidj18@gmail.com>
2023-03-17 22:59:38 -0600

    fixed key

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -10,6 +10,7 @@ export default function Timeline(props: Props) {
             {/* <div style={{ width: '10px', height: '100px', backgroundColor: '#E3C16F', position: 'absolute', left: '50%' }}></div> */}
             { props.entries.map((entry, i) => (
                 <TimelineEntry
+                key={i}
                 title={entry.title}
                 image={entry.image}
                 content={entry.content}
